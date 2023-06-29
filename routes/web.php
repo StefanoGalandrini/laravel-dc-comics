38a9fd69a8b350b7da9639baf6c0bfecf271723f
StefanoGalandrini <stefe68@gmail.com>
2023-06-29 22:15:07 +0200

    Fix code, "Create" still not working

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,4 @@ Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-Route::post(
-    '/comics',
-    [ComicController::class, 'store']
-)->name('comics.store');
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
