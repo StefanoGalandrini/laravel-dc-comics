abf163fbcdd0577ea111157bbe7b8f28383df46c
StefanoGalandrini <stefe68@gmail.com>
2023-06-30 01:12:01 +0200

    Add style for "show"

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,33 +1,46 @@
 @extends('layouts.base')
 
 @section('main')
-	<div class="container mt-5">
+	<div class="container mt-2">
 		<h1>{{ $comic->title }}</h1>
 		<a class="btn btn-primary" href="{{ route('home') }}">View</a>
 
-		<div class="mb-3">
-			<img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+		<div class="content">
+			<div class="image-container">
+				<img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+			</div>
+			<div class="text-container">
+				<p class="description info-text">Description: <span class="info-value">{{ $comic->description }}</span></p>
+
+				<div class="info">
+					<div class="left">
+						<p class="info-text">Price: <span class="info-value">{{ $comic->price }}</span></p>
+						<p class="info-text">Series: <span class="info-value">{{ $comic->series }}</span></p>
+						<p class="info-text">Sale Date: <span class="info-value">{{ $comic->sale_date }}</span></p>
+						<p class="info-text">Type: <span class="info-value">{{ $comic->type }}</span></p>
+					</div>
+					<div class="middle">
+						<p class="info-text">Artists:</p>
+						<ul>
+							@foreach ($comic->artists as $artist)
+								<li>
+									<span class="info-value">{{ $loop->last ? $artist : $artist . ', ' }}</span>
+								</li>
+							@endforeach
+						</ul>
+					</div>
+					<div class="right">
+						<p class="info-text">Writers:</p>
+						<ul>
+							@foreach ($comic->writers as $writer)
+								<li>
+									<span class="info-value">{{ $loop->last ? $writer : $writer . ', ' }}</span>
+								</li>
+							@endforeach
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
-		<p><strong>Description:</strong> {{ $comic->description }}</p>
-		<p><strong>Price:</strong> {{ $comic->price }}</p>
-		<p><strong>Series:</strong> {{ $comic->series }}</p>
-		<p><strong>Sale Date:</strong> {{ $comic->sale_date }}</p>
-		<p><strong>Type:</strong> {{ $comic->type }}</p>
-		<p><strong>Artists:</strong></p>
-		<ul>
-			@foreach ($comic->artists as $artist)
-				<li>
-					{{ $loop->last ? $artist : $artist . ', ' }}
-				</li>
-			@endforeach
-		</ul>
-		<p><strong>Writers:</strong></p>
-		<ul>
-			@foreach ($comic->writers as $writer)
-				<li>
-					{{ $loop->last ? $writer : $writer . ', ' }}
-				</li>
-			@endforeach
-		</ul>
 	</div>
 @endsection
